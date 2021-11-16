f9fa38808707e991b6525b0a0ed2c00a5e37a6e6
codenius <94353572+codenius@users.noreply.github.com>
2021-11-16 15:13:27 +0100

    Bug fixes to loading animation

--- frontend/js/loadingAnimation.js
+++ frontend/js/loadingAnimation.js
@@ -1,8 +1,8 @@
 function loadingAnimation() {
     document.getElementById('iframe').addEventListener('load', function () {
         // Hide the loading indicator
-        document.getElementById('loadingAnimation').style.opacity = '0';
-        setTimeout(overlay_off(), 1000);
+        // document.getElementById('loadingAnimation').style.opacity = '0';
+        setTimeout(overlay_off, 1000);
 
         // Bring the iframe back
         document.getElementById('iframe').style.opacity = '1';
@@ -10,6 +10,6 @@ function loadingAnimation() {
 
     document.getElementById('iframe').style.opacity = '0';
     overlay_on("<iframe src='loadingAnimation.html' id='loadingAnimation' width='100%' height='100%'>");
-    document.getElementById("loadingAnimation").style.opacity = '1';
+    // document.getElementById("loadingAnimation").style.opacity = '1';
 
 };
