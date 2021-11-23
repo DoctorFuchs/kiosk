96eb74212ac5c48aabbba71bc63904f7b955c9f5
DoctorFuchs <fuchs9203@gmail.com>
2021-11-23 07:21:14 +0100

    a little bit faster backend

--- backend/webserver.py
+++ backend/webserver.py
@@ -51,24 +51,16 @@ class Webserver(server.SimpleHTTPRequestHandler):
         self.send_response(200)
         if self.path.endswith(".png"):
             self.send_header("Content-type", "image/png")
-        
-        if self.path.endswith(".html"):
-            self.send_header('Content-type', 'text/html;encoding=UTF-8')
-
         self.end_headers()
-
+        #self.wfile = open("test.txt", "w+b")
         path = getFrontendPath()
 
         self.path = self.path.replace("/", os.sep)
         try:
-            if self.path.endswith(".png"):
-                self.wfile.write(open(path+self.path, "rb").read())
-
-            else:
-                self.wfile.write(bytes("".join(open(path + self.path, "rt").readlines()), "utf-8"))
+            self.wfile.write(open(path+self.path, "rb").read())
 
         except (FileNotFoundError, IsADirectoryError, OSError):
-            self.wfile.write(bytes("".join(open(path + os.sep + "index.html", "rt").readlines()), "utf-8"))
+            self.wfile.write(open(path + os.sep + "index.html", "rb").read())
 
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
