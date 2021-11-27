e7b3837891fa323abc66a7e641ddd8e2fb74f863
DoctorFuchs <fuchs9203@gmail.com>
2021-11-27 16:42:20 +0100

    finally removed tutorial

--- frontend/js/overlay.js
+++ frontend/js/overlay.js
@@ -3,18 +3,6 @@ var db_error_overlay_text = "<div>" +
     "<p style='color:red'>Bitte prüfen Sie die backend konsole für mehr Informationen.<br>Überprüfen Sie zudem, ob Ihre Konfigurationen richtig sind</h1>" +
     "</div>"
 
-var tutorial_overlay_slide_1 = "<div>" +
-    "<h1>Tutorial</h1>" +
-    "<p>Willkommen beim Tutorial<br>Wenn Sie fragen zu diesem Programm haben, wenden Sie sich bitte an github.com/doctorfuchs" +
-    "<br></p>" +
-    "<button onclick='overlay_on(tutorial_overlay_slide_2); document.getElementById(\"nav\").classList.add(\"tutorial-foreground\")'>Let's go</button><button onclick='overlay_off(), removeClassesOnObejects(\"tutorial-foreground\")'>Exit</button></div>"
-
-var tutorial_overlay_slide_2 = "<div style='transform: translate(-50vw, -40vh)'>" +
-    "<h1>Tutorial: Schritt 1 - Navigationsbar</h1>" +
-    "<p>Das ist die Navigationsbar.<br> Wenn Sie in ein anderes Menü wollen, dann kommen Sie hier am schnellsten voran." +
-    "<br></p>" +
-    "<button onclick='overlay_off(); removeClassesOnObejects(\"tutorial-foreground\")'>Exit</button></div>"
-
 function overlay_on(content) {
     document.getElementById("overlay").innerHTML = content;
     document.getElementById("overlay").style.display = "block";
@@ -31,8 +19,4 @@ function removeClassesOnObejects(class_name) {
     while (matches.length > 0) {
         matches[0].classList.remove(class_name);
     }
-}
-
-function tutorial() {
-    overlay_on(tutorial_overlay_slide_1)
 }
