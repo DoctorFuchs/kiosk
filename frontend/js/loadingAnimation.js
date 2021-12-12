02f9f3b38a97c0d98ea5c94a8209ffa3469f4a1e
codenius <94353572+codenius@users.noreply.github.com>
2021-12-12 16:11:37 +0100

    Added special christmas loading animation

--- frontend/js/loadingAnimation.js
+++ frontend/js/loadingAnimation.js
@@ -1,15 +1,16 @@
 function loadingAnimation() {
     document.getElementById('iframe').addEventListener('load',() => {
         // Hide the loading indicator
-        // document.getElementById('loadingAnimation').style.opacity = '0';
         setTimeout(overlay_off, 1000);
-
         // Bring the iframe back
         document.getElementById('iframe').style.opacity = '1';
     });
-
     document.getElementById('iframe').style.opacity = '0';
-    overlay_on("<iframe src='loadingAnimation.html' id='loadingAnimation' width='100%' height='100%'>");
-    // document.getElementById("loadingAnimation").style.opacity = '1';
-
+    var month = new Date().getMonth() + 1
+    if(month === 12) {
+        var loadingAnimation = "christmasLoadingAnimation.html"
+    } else {
+        var loadingAnimation = "loadingAnimation.html"
+    }
+    overlay_on(`<iframe src='${loadingAnimation}' id='loadingAnimation' width='100%' height='100%'>`);
 };
