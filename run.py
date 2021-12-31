951c7ac48aa861cd224213b60e4068e27c8d3071
codenius <94353572+codenius@users.noreply.github.com>
2021-12-31 18:04:53 +0100

    Optimized run.py

--- run.py
+++ run.py
@@ -1,31 +1,51 @@
 import argparse
-import sys, os, threading
-import webbrowser as web
-from time import sleep
+import os
+import subprocess
+import sys
+parser = argparse.ArgumentParser(description="Launcher for kiosk application")
+parser.add_argument("-u", "--upgrade", help="Upgrade flask and it's dependencys, stored in lib folder", action="store_true")
+parser.add_argument("-b", "--browser", help="Launch browser while starting", action="store_true")
+parser.add_argument("-f", "--fullscreen", help="Launch browser fullscreen (chrome need to be installed, works only with -b/--browser, exit with Alt+F4)", action="store_true")
+args = parser.parse_args()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Launcher for kiosk application")
-    parser.add_argument("-b", "--browser", help="Launch browser while starting", action="store_true")
-    # At the moment in development
-    # parser.add_argument("-f", "--fullscreen", help="Launch browser fullscreen (chrome need to be installed, exit with Alt+F4)", action="store_true") 
-    args = parser.parse_args()
+dir = os.path.split(__file__)[0]
+sys.path.append(os.path.join(dir, "lib"))
 
+def upgradeDependencys():   #not working with autoreload of flask
     try:
-        __import__("flask")
-
+        import pip
     except ImportError:
-        # retry after installing
-        os.system("python3 -m pip install flask")
-        sys.exit("Please restart the program, because flask is now installed")
+        subprocess.check_call([sys.executable, "-m", "ensurepip"])
+    subprocess.check_call([sys.executable ,"-m" , "pip", "install", "flask", "-t", os.path.join(dir, "lib"), "--upgrade"])
+    # import pip
+    # pip.main(["install", "flask", "-t", os.path.join(dir, "lib"), "--upgrade"])
+    import flask
+    print("\nStarting now...\n")
 
-    if args.browser:
-        def startWebbrowser():
-            sleep(1) # with delay, because the server is not started
-            web.open_new("http://localhost:1024")
+if args.upgrade:
+    upgradeDependencys()
 
-        threading.Thread(target=startWebbrowser).start()
-        
+try:
+    __import__("flask")
+except ImportError:
+    try:
+        upgradeDependencys()
+    except ImportError:
+        print("Unable to install dependencies. Please install flask and it's dependencies manually (e.g. pip3 install flask).")
+        sys.exit()
 
-    sys.path.append(__file__.replace("run.py", "backend"))
-    # starts the server 
-    from backend import server
+if args.browser:
+    def openBrowser():
+        import webbrowser 
+        webbrowser.open_new("http://localhost:1024")    #Dont now why it opens two tabs sometimes???
+    try:
+        if args.fullscreen:
+            subprocess.Popen(["C:\Program Files\Google\Chrome\Application\chrome.exe", "http://localhost:1024", "--kiosk"])   #Add cross-platform support
+        else:
+            openBrowser()
+    except FileNotFoundError:
+        openBrowser()
+
+
+sys.path.append(os.path.join(dir, "backend"))
+from backend import server
