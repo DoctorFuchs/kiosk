f8bc7db1b3a1b42e7d89f8bd7913b85160cf4003
codenius <94353572+codenius@users.noreply.github.com>
2022-03-04 18:56:05 +0100

    Mark active tab in navbar

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -3,9 +3,25 @@ function countChildren(element) {
 	return relem.childNodes.length;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelectorAll(".right-navbar-item > li > a[href], .dashboard_nav > ul > a").forEach((element) => {
+		element.addEventListener("click", (event) => {
+			event.preventDefault()
+			gotosite(event.target.href)
+		})
+	})
+})
+
 function gotosite(site) {
-	var content = document.getElementById("content");
-	content.innerHTML = `<iframe src='${site}' frameborder='0' width='100%' height='100%' id='iframe' style='opacity:0; transition:opacity 0.3s ease;'>`;
+	document.querySelectorAll(".right-navbar-item > li > a").forEach((element) => {
+		element.classList.remove("active")
+	})
+	document.querySelectorAll(`.right-navbar-item > li > a`).forEach((element) => {
+		if (element.href == site) {
+			element.classList.add("active")
+		}
+	})
+	document.getElementById("content").innerHTML = `<iframe src='${site}' frameborder='0' width='100%' height='100%' id='iframe' style='opacity:0; transition:opacity 0.3s ease;'>`;
 	loadingAnimation();
 }
 
@@ -13,9 +29,8 @@ function toggleFullScreen() {
 	if (!document.fullscreenElement) {
 		document.documentElement.requestFullscreen();
 	} else {
-	  if (document.exitFullscreen) {
-		document.exitFullscreen();
-	  }
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		}
 	}
-  }
-
+}
