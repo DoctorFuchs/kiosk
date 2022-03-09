b4123ef66c4eb9b7f735af9894580e70b110801f
codenius <94353572+codenius@users.noreply.github.com>
2022-03-09 18:04:34 +0100

    Fixed spaces in German about

--- backend/languages.py
+++ backend/languages.py
@@ -1,79 +1,74 @@
 languages = {
     "de": {
-        "title":"Kiosk",
-        "shop":"Shop",
-        "storage":"Lager",
-        "about":"\u00DCber",
-        "new_product":"Neues Produkt",
-        "amount_available":"Verf\u00FCgbare Menge",
-        "price":"Preis",
-        "bill":"Einkaufszettel",
-        "fullscreen":"Vollbild",
+        "title": "Kiosk",
+        "shop": "Shop",
+        "storage": "Lager",
+        "about": "\u00DCber",
+        "new_product": "Neues Produkt",
+        "amount_available": "Verf\u00FCgbare Menge",
+        "price": "Preis",
+        "bill": "Einkaufszettel",
+        "fullscreen": "Vollbild",
         "name": "Name",
         "creation": "Erstellung",
-        "editing":"Bearbeitung",
-        "delete":"L\u00F6schen",
+        "editing": "Bearbeitung",
+        "delete": "L\u00F6schen",
         "accept": "Okay",
-        "payed": "Bezahlt", 
-        "language":"Sprache",
-        "save":"Speichern",
-        "cancel":"Abbrechen",
-        "add_products_warning":"F\u00FCgen Sie Produkte durch anklicken hinzu",
-        "pay":"Abschlie\u00DFen",
-        "settings":"Einstellungen",
+        "payed": "Bezahlt",
+        "language": "Sprache",
+        "save": "Speichern",
+        "cancel": "Abbrechen",
+        "add_products_warning": "F\u00FCgen Sie Produkte durch anklicken hinzu",
+        "pay": "Abschlie\u00DFen",
+        "settings": "Einstellungen",
         "about_attributes": {
-            "header":"\u00DCber dieses Projekt",
+            "header": "\u00DCber dieses Projekt",
             "bodys": [
-                "Diese Anwendung ist ein einfaches Kassenystem f\u00FCr kleine Kioske, St\u00E4nde und Buden. Das Backend ist in"+
-            "Python geschrieben, das Frontend mit den drei Webdesign-Technologien HTML, CSS und JavaScript programmiert."+
-            "Die Kommunikation zwischen ihnen wird durch einen Webserver mit Flask sichergestellt. Sie sind eingeladen,"+
-            "das System zu verbessern und zum Projekt beizutragen.",
-                "Haben sie Fragen, Probleme oder einen Verbesserungsvorschlag, dann wenden sie sich an ihren Administrator"+
-            "oder \u00F6ffnen sie ein Issue auf auf der GitHub-Seite."    
+                "Diese Anwendung ist ein einfaches Kassenystem f\u00FCr kleine Kioske, St\u00E4nde und Buden. Das Backend ist in Python geschrieben, das Frontend mit den drei Webdesign-Technologien HTML, CSS und JavaScript programmiert. Die Kommunikation zwischen ihnen wird durch einen Webserver mit Flask sichergestellt. Sie sind eingeladen, das System zu verbessern und zum Projekt beizutragen. Haben sie Fragen, Probleme oder einen Verbesserungsvorschlag, dann wenden sie sich an ihren Administrator oder \u00F6ffnen sie ein Issue auf auf der GitHub-Seite."
             ]
-            },
+        },
         "greeting_attributes": {
-            "header":"Anscheinend ist dies das erste Mal, dass dieses Programm ausgef\u00FChrt wird.",
+            "header": "Anscheinend ist dies das erste Mal, dass dieses Programm ausgef\u00FChrt wird.",
             "bodys": [
                 "Bitte konfigurieren Sie ihre Kontaktinformation wie in der Dokumentation beschrieben.",
                 "Passen sie gegebenenfalls auch die anderen Programmeinstellungen an."
-                ]
-            }
+            ]
+        }
     },
     "eng": {
-        "title":"Kiosk",
-        "shop":"Shop",
-        "storage":"Storage",
-        "about":"About",
-        "new_product":"New product",
-        "amount_available":"Amount available",
-        "price":"Price",
-        "bill":"Bill",
+        "title": "Kiosk",
+        "shop": "Shop",
+        "storage": "Storage",
+        "about": "About",
+        "new_product": "New product",
+        "amount_available": "Amount available",
+        "price": "Price",
+        "bill": "Bill",
         "name": "Name",
         "creation": "Creation",
-        "editing":"Editing",
-        "delete":"Delete",
+        "editing": "Editing",
+        "delete": "Delete",
         "accept": "Accept",
-        "cancel":"Cancel",
-        "add_products_warning":"Add products by clicking on them",
-        "pay":"pay",
-        "language":"Language",
-        "save":"Save",
-        "settings":"Settings",
-        "payed":"payed",
-        "fullscreen":"Fullscreen",
+        "cancel": "Cancel",
+        "add_products_warning": "Add products by clicking on them",
+        "pay": "pay",
+        "language": "Language",
+        "save": "Save",
+        "settings": "Settings",
+        "payed": "payed",
+        "fullscreen": "Fullscreen",
         "about_attributes": {
-            "header":"About this project",
+            "header": "About this project",
             "bodys": [
                 "This project is a simple cashbox system. We build this tool with python3 (flask) for backend and html, js and css.",
-                "If you have any questions, suggestions, ... open an issue at github (link below)"    
+                "If you have any questions, suggestions, ... open an issue at github (link below)"
             ]
-            },
+        },
         "greeting_attributes": {
-            "header":"It's your first time here, isn't it?",
+            "header": "It's your first time here, isn't it?",
             "bodys": [
                 "Please configurate this program. (If you need help, take a look into out README)"
-                ]
-            }
+            ]
+        }
     },
 }
